3a1cacda2eaa0c972182d14b03a1858ceed60f14
Maksym Mukanovskyi <maksymmukanovskyi@Maksyms-MacBook-Pro.local>
2022-10-18 16:28:36 +0300

    initial commit

---
+++ JS_Challenges/BasicChallenge_18_10_22/18_10_22/index.js
@@ -0,0 +1,17 @@
+'use stict';
+const bmi = function (height, mass) {
+  return mass / Math.pow(height, 2);
+};
+const Mark = {
+  mass: 78,
+  height: 1.69,
+};
+const John = {
+  mass: 92,
+  height: 1.95,
+};
+
+const markHigherBMI =
+  Math.round(bmi(Mark.height, Mark.mass)) >
+  Math.round(bmi(John.height, John.mass));
+console.log(markHigherBMI);
