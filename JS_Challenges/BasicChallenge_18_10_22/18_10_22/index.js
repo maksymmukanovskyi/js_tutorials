864cfa97f0ddbcc35e7215681263a02be90e331d
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2022-10-19 13:14:55 +0300

    new commit 19/10/22

--- JS_Challenges/BasicChallenge_18_10_22/18_10_22/index.js
+++ JS_Challenges/BasicChallenge_18_10_22/18_10_22/index.js
@@ -15,7 +15,26 @@ const markHigherBMI =
   Math.round(bmi(Mark.height, Mark.mass)) >
   Math.round(bmi(John.height, John.mass));
 console.log(markHigherBMI);
-//////////////// Next challenge/////////////////////
-//////////////// Next challenge/////////////////////
-//////////////// Next challenge/////////////////////
-//////////////// Next challenge/////////////////////
+///////////////////////////challenge 3/////////
+
+const averageNum = function (...args) {
+  return args.reduce((acc, el) => (acc += el), 0) / args.length;
+};
+
+function checkWinner(teamA, teamB) {
+  if (teamA <= 2 * teamB) {
+    console.log('Team B wins');
+  } else if (teamA >= 2 * teamB) {
+    console.log('Team A wins');
+  } else {
+    console.log('equal');
+  }
+}
+
+/*
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+*/
+const dolf = averageNum(85, 54, 41);
+const koal = averageNum(23, 34, 27);
+checkWinner(dolf, koal);
