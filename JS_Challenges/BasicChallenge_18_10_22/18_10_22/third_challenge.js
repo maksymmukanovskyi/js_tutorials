5f22461fec4872fe0fdef43db7e95aa281c10e65
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2022-10-29 13:53:44 +0300

    finishing strings methods

--- JS_Challenges/BasicChallenge_18_10_22/18_10_22/third_challenge.js
+++ JS_Challenges/BasicChallenge_18_10_22/18_10_22/third_challenge.js
@@ -163,3 +163,23 @@ function onClickBtn(e) {
   console.log(...result);
   text.value = '';
 }
+///////////////////// String methods practice ////////////////////////
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const toUpperCode = str => str.slice(0, 3).toUpperCase();
+for (const flightStr of flights.split('+')) {
+  const [type, from, to, time] = flightStr.split(';');
+  const displayedStr = `${
+    type.startsWith('_Delayed') ? '🔴' : ''
+  } ${type.replaceAll('_', ' ')} from ${toUpperCode(from)} to ${toUpperCode(
+    to
+  )} (${time.replace(':', 'h')})`.padStart(36);
+  console.log(displayedStr);
+}
