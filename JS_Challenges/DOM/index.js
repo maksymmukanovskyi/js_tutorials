bff8abbe8dc4606f4b49942ecf93f23583af20b8
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2022-11-12 01:29:28 +0200

    slider dev.

--- JS_Challenges/DOM/index.js
+++ JS_Challenges/DOM/index.js
@@ -176,6 +176,45 @@ const imageObserver = new IntersectionObserver(loadingImg, {
 
 imageTargets.forEach(img => imageObserver.observe(img));
 
+// Slides
+const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+let currentSlide = 0;
+
+const nextSlide = function () {
+  if (currentSlide === maxSlide - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  goToSlide(currentSlide);
+};
+
+const prevSlide = function () {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide - 1;
+  } else {
+    currentSlide--;
+  }
+  goToSlide(currentSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
 /*
 
 CHALLANGES///////////////////// / / / / / //  
