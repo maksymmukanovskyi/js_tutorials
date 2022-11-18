77db27db54bb0d88c3623495b6af16926c5aaad4
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2022-11-18 17:39:04 +0200

    geolocation with leaflet

--- mapty/script.js
+++ mapty/script.js
@@ -10,3 +10,49 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map, mapEvent;
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    pos => {
+      const { latitude } = pos.coords;
+      const { longitude } = pos.coords;
+      const cords = [latitude, longitude];
+
+      map = L.map('map').setView(cords, 13);
+      console.log(map);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', mapEvt => {
+        mapEvent = mapEvt;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+    },
+    () => {
+      alert('Could not get your position');
+    }
+  );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
