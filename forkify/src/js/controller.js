7dd9496343f4ab23a8ef8d31c76da52f0d714a39
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2022-12-09 18:52:51 +0200

    finished project settings

--- forkify/src/js/controller.js
+++ forkify/src/js/controller.js
@@ -1,3 +1,5 @@
+import '../sass/main.scss';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -7,7 +9,37 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
+console.log(10);
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const responce = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await responce.json();
+    console.log(data);
+    if (!responce.ok) throw new Error(`Fuck!!`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      imge: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+showRecipe();
---
+++ forkify/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/js/controller.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    // filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+  },
+  plugins: [
+    new HtmlWebPlugin({
+      filename: 'index.html',
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'style.css',
+      chunkFilename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: '/.js$/',
+        exclude: '/node_modules/',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+};
